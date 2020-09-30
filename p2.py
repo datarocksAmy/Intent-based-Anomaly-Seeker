d93d43a30c91ee805c74c74232fcf1ad6766eee5
Amy <Amy.Lin@ipfs.com>
2020-09-30 17:55:39 -0500

    doc string for aggregating intent outliers + init draft for p2 main

--- p2.py
+++ p2.py
@@ -27,28 +27,49 @@ python p2.py data.json
 """
 
 import json
+from os import getcwd
+from sys import argv
 
+from data_silo.data_processing import DataProcessing
 from model_brewery.d2v_distillery import BrewModel
 
 
 def aggregate_intent_outlier(data_df):
-    outlier_dict = {}
-    unique_intent_list = list(set(data_df["Intent"]))
-    for intent_key in unique_intent_list:
-        subset_intent_outlier = data_df[data_df["Intent"] == intent_key]
-        outlier_dict["intent_key"] = list(subset_intent_outlier["Query"])
+    """
+    Group Query by Intent + Doc2Vec Score below score to indicate possible anomaly.
+    Dump into json file as the final step.
 
-    return outlier_dict
+    :param data_df:     ( DataFrame ) Data of Doc2Vec Intent not the same as Original Intent
+    """
+
+    # Init Outlier Dictionary
+    outlier_dict = {}
+    # Get the list of unique Intents
+    unique_intent_list = list(set(data_df["Intent"]))
+    # Group by Intent w/ Doc2Vec Score below threshold
+    for intent_key in unique_intent_list:
+        subset_intent_outlier = data_df[(data_df["Intent"] == intent_key) & (data_df["Doc2Vec_Score"] < 0.6)]
+        # Get the list of possible outlier queries
+        outlier_dict[intent_key] = list(subset_intent_outlier["Query"])
+    # Dump result to JSON file
+    with open("outlier.json", "w") as outlier_file:
+        json.dump(outlier_dict, outlier_file, indent=4)
 
 
 def main():
-    # DataProcessing(argv[1]).clean_text()
-    data_tag_DF = BrewModel("data_lake/data.json").brew_tags()
-    data_tag_DF["IsSame"] = (data_tag_DF["Intent"] == data_tag_DF["Doc2Vec_Intent"])
-    outliers_DF = data_tag_DF[data_tag_DF["IsSame"] == True].reset_index(drop=True)
-    agg_outliers_dict = aggregate_intent_outlier(outliers_DF)
-    with open("outlier.json", "w") as outlier_file:
-        json.dump(agg_outliers_dict, outlier_file, indent=4)
+    # Input JSON data
+    incoming_json_data = f"{getcwd()}/data_lake/{argv[1]}"
+    # Pre-process texts
+    DataProcessing(incoming_json_data).clean_text()
+    # Build Doc2Vec Model
+    BrewModel(incoming_json_data).brew_doc2vec()
+    # Assign New Intents to original data with scores
+    data_tag_DF = BrewModel(incoming_json_data).brew_tags()
+    # Get the queries w/ a different tag being assigned by Doc2Vec
+    outliers_DF = data_tag_DF[data_tag_DF["Intent"] != data_tag_DF["Doc2Vec_Intent"]].reset_index(drop=True)
+    # Aggregate potential outlier queries with score less than 0.4
+    aggregate_intent_outlier(outliers_DF)
+
 
 if __name__== "__main__":
     main()
